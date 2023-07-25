9105801559cbf6bd1f8c16ae24616520ef88a5a8
WetFlamer <xamidchabaev04@gmail.com>
2023-07-25 18:17:48 +0300

    Сделаны все нужные виджеты

--- wordpress/wp-content/themes/sportisland/functions.php
+++ wordpress/wp-content/themes/sportisland/functions.php
@@ -1,26 +1,33 @@
 <?php
 
 $widgets = [
-'widget-text.php',
-'widget-contacts.php',
-'widget-social.php',
-'widget-map.php'
+    'widget-text.php',
+    'widget-contacts.php',
+    'widget-social.php',
+    'widget-map.php',
+    'widget-info.php'
 
 ];
 
-foreach($widgets as $w) {
+foreach ($widgets as $w) {
     require_once(__DIR__ . '/inc/' . $w);
 }
 
 add_action('after_setup_theme', 'si_setup');
 add_action('wp_enqueue_scripts', 'si_scripts');
+add_action('widgets_init', 'si_register');
+
+add_shortcode('si-paste-link', 'si_paste_link');
+
 add_filter('show_admin_bar', '__return_false');
-add_action( 'widgets_init', 'si_register' );
+add_filter('si_widget_text', 'do_shortcode');
+
+
 
 function si_setup()
 {
-    register_nav_menu( 'menu-header', 'Меню в шапке' );
-    register_nav_menu( 'menu-footer', 'Меню в подвале' );
+    register_nav_menu('menu-header', 'Меню в шапке');
+    register_nav_menu('menu-footer', 'Меню в подвале');
     add_theme_support('custom-logo');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
@@ -45,7 +52,8 @@ function si_scripts()
 
 }
 
-function si_register() {
+function si_register()
+{
     register_sidebar([
         'name' => 'Контакты в шапке сайта',
         'id' => 'si_header',
@@ -89,12 +97,46 @@ function si_register() {
         'after_widget' => null
     ]);
 
-    register_widget( 'si_widget_text' );
+    register_widget('si_widget_text');
     register_widget('si_widget_contacts');
     register_widget('si_widget_social');
     register_widget('si_widget_iframe');
+    register_widget('si_widget_info');
 
 }
+
+function si_paste_link($attr)
+{
+    $params = shortcode_atts([
+        'link' => '',
+        'text' => '',
+        'type' => 'link'
+    ], $attr);
+    $params['text'] = $params['text'] ? $params['text'] : $params['link'];
+    if ($params['link']) {
+        $protocol = '';
+        switch ($params['type']) {
+            case 'email':
+                $protocol = 'mailto:';
+                break;
+            case 'phone':
+                $protocol = 'tel:';
+                $params['link'] = preg_replace('/[^+0-9]/', '', $params['link']);
+                break;
+
+            default:
+                $protocol = '';
+                break;
+        }
+        $link = $protocol . $params['link'];
+        $text = $params['text'];
+
+        return "<a href=\"${link}\">${text}</a>";
+    } else {
+        return '';
+    }
+}
+;
 
 function _si_assets_path($path)
 {
